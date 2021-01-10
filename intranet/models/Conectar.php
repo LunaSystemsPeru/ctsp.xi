3d8ef2fb088242357fe4d95fa38d64feafe136f9
Administrador <emantillag@hotmail.com>
2021-01-10 10:21:40 -0500

    cambio noticias por eventos

--- intranet/models/Conectar.php
+++ intranet/models/Conectar.php
@@ -6,7 +6,7 @@ class Conectar
 
     private static $_instancia;
     private $_connection;
-    private $_host = "localhost";
+    private $_host = "192.168.37.9";
     private $_user = "root_lsp";
     private $_pass = "root/*123";
     // Almacenar una unica instancia
