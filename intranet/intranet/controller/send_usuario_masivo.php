cab256a2f3b154a17342d4e0a8ccf6f33ccc3b03
Luis Oyanguren <leog.1992@gmail.com>
2021-01-12 09:59:44 -0500

    ULTIMOS CAMBIOS DE URGENCIA NO ME ACUERDO

--- intranet/intranet/controller/send_usuario_masivo.php
+++ intranet/intranet/controller/send_usuario_masivo.php
@@ -10,9 +10,18 @@ $array_asociados_0 = $asociado->verAsociados();
 
 foreach ($array_asociados_0 as $item) {
     if ($item['email'] != "") {
-        $asociado->setIdAsociado($item['id_asociado']);
+        /*$asociado->setIdAsociado($item['id_asociado']);
         $asociado->setPassword($util->generarCodigo(8));
-        $asociado->actualizar();
+        $asociado->actualizar();*/
+        $email->setAsunto("ACCESO A PERFIL COLEGIADO");
+        $email->setCorreo($item['email']);
+        $email->setNombreReceptor($item['apellidos'] . " " . $item['nombres']);
+        $host= $_SERVER["HTTP_HOST"];
+        $url_server = "http://" . $host . "/clientes/ctsp.xi";
+        $url_email = $url_server ."/intranet/plantilla_emails/emaul_usuario_masivo.php?idasociado=" . $item['id_asociado'];
+        $email->setUrl($url_email);
+        //echo $url_email;
+        $email->EnviarEmail();
     }
 }
 
@@ -21,8 +30,17 @@ $array_asociados_1 = $asociado->verAsociados();
 
 foreach ($array_asociados_1 as $item) {
     if ($item['email'] != "") {
-        $asociado->setIdAsociado($item['id_asociado']);
+        /*$asociado->setIdAsociado($item['id_asociado']);
         $asociado->setPassword($util->generarCodigo(8));
-        $asociado->actualizar();
+        $asociado->actualizar();*/
+        $email->setAsunto("ACCESO A PERFIL COLEGIADO");
+        $email->setCorreo($item['email']);
+        $email->setNombreReceptor($item['apellidos'] . " " . $item['nombres']);
+        $host= $_SERVER["HTTP_HOST"];
+        $url_server = "http://" . $host . "/clientes/ctsp.xi";
+        $url_email = $url_server ."/intranet/plantilla_emails/emaul_usuario_masivo.php?idasociado=" . $item['id_asociado'];
+        $email->setUrl($url_email);
+        //echo $url_email;
+        $email->EnviarEmail();
     }
 }
