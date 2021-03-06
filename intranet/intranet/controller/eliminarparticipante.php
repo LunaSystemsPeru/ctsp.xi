c8ccf7578564f36e7feab77ca5b80beb4d0170d7
Administrador <emantillag@hotmail.com>
2021-03-06 00:55:33 -0500

    termino eliminar participante

--- intranet/intranet/controller/eliminarparticipante.php
+++ intranet/intranet/controller/eliminarparticipante.php
@@ -1,103 +1,10 @@
 <?php
-if (filter_input(INPUT_GET, 'id_participante')) {
-    $participante = filter_input(INPUT_GET, 'id_participante');
-}else{
-    header("location: ../contents/ver_cursos.php");//en caso no reciba valor, regresar a cursos
 
-}
+    $id_participante = filter_input(INPUT_GET, 'id_participante');
+
 require '../../models/CursosParticipante.php';
 $participante  = new CursosParticipante();
-$participante->setIdCurso($idcurso);//asignar el valor del idcurso
-
-require '../../models/CursosParticipante.php';
-
-
-$participante = new CursosParticipante();
-
-$asociado->generarCodigo();
-$asociado->setTipoDocumento(filter_input(INPUT_POST, 'select_tipo_documento'));
-$asociado->setDni(filter_input(INPUT_POST, 'input_dni'));
-$asociado->setApellido(filter_input(INPUT_POST, 'input_apellidos'));
-$asociado->setNombre(filter_input(INPUT_POST, 'input_nombres'));
-$asociado->setTipoRegistro(filter_input(INPUT_POST, 'select_tipo_inscripcion'));
-$asociado->setEmail(filter_input(INPUT_POST, 'input_email'));
-$asociado->setCelular(filter_input(INPUT_POST, 'input_celular'));
-$asociado->setDomicilio(filter_input(INPUT_POST, 'input_domicilio'));
-$asociado->setFechaNac(filter_input(INPUT_POST, 'input_fec_nac'));
-$asociado->setCentroTrabajo(filter_input(INPUT_POST, 'input_trabajo'));
-$asociado->setEstado(0);
-//0 = pendiente
-//1 = aprobado
-//2 = rechazado por corregir
-
-//datos en blanco
-$asociado->setUltimoPago("1000-01-01");
-$asociado->setFechaCertificado("1000-01-01");
-$asociado->setFechaInscripcion(date("Y-m-d"));
-
-//cargar files
-
-$array_files[] = Array (
-    "file" => "input_file_perfil",
-    "directorio" => "perfil"
-);
-$array_files[] = Array (
-    "file" => "file_ficha_inscripcion",
-    "directorio" => "ficha_inscripcion"
-);
-$array_files[] = Array (
-    "file" => "file_sunedu",
-    "directorio" => "sunedu"
-);
-$array_files[] = Array (
-    "file" => "file_titulo",
-    "directorio" => "titulo"
-);
-
-foreach ($array_files as $item) {
-    $directorio = "../../../images/asociados/" . $item['directorio'] . "/";
-    if (!file_exists($directorio)) { //verificar si existe carpeta
-        if (!mkdir($directorio, 0777, true)) { //sino existe crearla
-            die('Fallo al crear las carpetas...'); //si no se puede crear lanzar erro
-        }
-    }
-
-    $afile = $_FILES[$item['file']];
-
-    if(isset($afile)) {
-
-        $file = $afile['name'];
-        $file_temporal = $afile['tmp_name'];
-        $temporary = explode(".", $afile["name"]);
-        $file_extension = end($temporary);
-
-        if ($afile["error"] > 0) {
-            echo "Return Code: " . $afile["error"] . "<br/><br/>";
-        } else {
-            $new_name = $asociado->getIdAsociado() . '.' . $file_extension; //renonbrar archivo
-
-            if (move_uploaded_file($file_temporal, $directorio . $new_name)) {
-                if ($item['directorio'] == "perfil") {
-                    $asociado->setFoto($new_name);
-                }
-                if ($item['directorio'] == "ficha_inscripcion") {
-                    $asociado->setFichaInscripcion($new_name);
-                }
-                if ($item['directorio'] == "sunedu") {
-                    $asociado->setRegistroSunedu($new_name);
-                }
-                if ($item['directorio'] == "titulo") {
-                    $asociado->setTitulo($new_name);
-                }
-            } else {
-                echo "error no se pudo copiar la imagen a la carpeta";
-                //header("Location: ../reg_productos.php?error=3");
-            }
-        }
-    }
-}
-$asociado->insertar();
-
-header("Location: ../contents/ver_asociados_pendientes.php");
-
+$participante->setIdParticipante($id_participante);//asignar el valor del idparticipante
+$participante->eliminar();
+header("location:../contents/ver_cursos_detalle.php");
 
