e771e74b5d44a5235977e77362bdd02226a5a912
luisog <leog.1992@gmail.com>
2022-01-14 22:16:17 -0500

    acceso login, bloqueo de usuario con estado 1

--- intranet/gestor_contenidos/controller/login.php
+++ intranet/gestor_contenidos/controller/login.php
@@ -1,5 +1,29 @@
 <?php
+session_start();
+require '../../models/Usuario.php';
+
+$usuario = new Usuario();
+
 $usuario = filter_input(INPUT_POST, 'inputUsuario');
 $pass = filter_input(INPUT_POST, 'inputPassword');
 
-header("Location: ../contents/index.php");
+if ($usuario->validarUsuario()) {
+    if ($usuario->obtenerDatos()) {
+        if ($pass == $usuario->getPassword()) {
+            if ($usuario->getEstado() == 0) {
+                $_SESSION['_logueado'] = true;
+                $_SESSION['idusuario'] = $usuario->getIdUsuario();
+                header("Location: ../contents/index.php");
+            } else {
+                //usuario bloqueado
+                header("Location: ../contents/login.php?error=2");
+            }
+        } else {
+            //contraseña incorrecta
+            header("Location: ../contents/login.php?error=1");
+        }
+    }
+} else {
+    //usuario no existe
+    header("Location: ../contents/login.php?error=2");
+}
